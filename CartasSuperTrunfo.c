6bf827307d4af2b8807d934d9ed278d56055c702
Atena-est <andreagfig.agf@gmail.com>
2025-03-17 15:18:47 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 int main() {
        int codigo = 1;
-    char estado[50] = "RIO DE JANEIRO"; 
-    char inicial[3] = "A0";
-    char nome[50] = "Rio de Janeiro";
-    float populacao = 6.21;
-    char populacaovalor[20] = "milhões";
-    float area = 1.26;
-    char extensaoarea[10]= "K km2";
-    float pib = 3.59;
-    char pib2 [15]= "milhões";
-    int turismo = 39;
-
-    printf("Código da Carta: %s%d\n", inicial,codigo);
-    printf("Estado: %s\n",estado);
-    printf("Cidade: %s\n", nome);
-    printf("População: %f %s\n", populacao,populacaovalor);
-    printf("Área: %f%s\n",area, extensaoarea);
-    printf("PIB: %f %s\n",pib, pib2);
-    printf("Pontos Turisticos:%d\n",turismo);
+       char estado[50] = "RIO DE JANEIRO"; 
+       char inicial[3] = "A0";
+       char nome[50] = "Rio de Janeiro";
+       float populacao = 6.21;
+       char populacaovalor[20] = "milhões";
+       float area = 1.26;
+       char extensaoarea[10]= "K km2";
+       float pib = 3.59;
+       char pib2 [15]= "milhões";
+       int turismo = 39;
+       
+       printf("Código da Carta: %s%d\n", inicial,codigo);
+       printf("Estado: %s\n",estado);
+       printf("Cidade: %s\n", nome);
+       printf("População: %f %s\n", populacao,populacaovalor);
+       printf("Área: %f%s\n",area, extensaoarea);
+       printf("PIB: %f %s\n",pib, pib2);
+       printf("Pontos Turisticos:%d\n",turismo);
     
     return 0;
 
