54cf3bd33e6454658ddf2d121ac1ae78f8f7cb50
Atena-est <andreagfig.agf@gmail.com>
2025-03-17 14:58:58 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 int main() {
-    
     int codigo = 1;
     char estado[50] = "RIO DE JANEIRO"; 
-    char inicial[3] = "A";
+    char inicial[3] = "A0";
     char nome[50] = "Rio de Janeiro";
     float populacao = 6.21;
     char populacaovalor[20] = "milhões";
@@ -12,13 +11,14 @@ int main() {
     float pib = 3.6;
     int turismo = 39;
 
-    
-    printf("Código da Carta: %s %d\n",codigo, inicial);
-    printf("Estado do: %s cidade: %s\n",estado, nome);
-    printf("População: %f %s\n",populacaovalor, populacao);
-    printf("Área:%f %s\n",extensaoarea, area);
-    printf("PIB:%f\n",pib);
+    printf("Código da Carta: %s%d\n", inicial,codigo);
+    printf("Estado: %s\n",estado);
+    printf("Cidade: %s\n", nome);
+    printf("População: %f %s\n", populacao,populacaovalor);
+    printf("Área: %f%s\n",area, extensaoarea);
+    printf("PIB: %f\n",pib);
     printf("Pontos Turisticos:%d\n",turismo);
 
     return 0;
+
 }
