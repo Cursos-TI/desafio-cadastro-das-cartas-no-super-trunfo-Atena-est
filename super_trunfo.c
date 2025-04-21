35ef4a9170260e0e2642c9723b8a0ada4c255b21
Atena-est <andreagfig.agf@gmail.com>
2025-04-21 18:39:03 -0300

    Update super_trunfo.c
    Adiciona o desafio nível aventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -15,7 +15,7 @@ int main() {
     char extensaoarea[10]= "K km2";
     float pib = 350;
     char pib2 [15]= "bilhões";
-    int turismo = 39;
+    int turismo = 40;
 
   
     //Carta2
@@ -34,23 +34,112 @@ int main() {
     //Comparando Cartas do Super Trunfo
 
     float densidadeCarta1, pibPerCapitaCarta1, densidadeCarta2, pibPerCapitaCarta2;
+    int atributo;
   
     densidadeCarta1 = populacao/area;
     pibPerCapitaCarta1 = pib/populacao;
     densidadeCarta2 = populacao2/area2;
     pibPerCapitaCarta2 = pib3/populacao2;
 
-    printf(" o pib per capita da carta1 é : %f\n", pibPerCapitaCarta1);
-    printf(" o pib per capita da carta2 é : %f\n", pibPerCapitaCarta2);
+    //Nível Aventureiro
 
-    if(pibPerCapitaCarta1 > pibPerCapitaCarta2){
-        printf ("carta 1 Rio de Janeiro venceu");
+    printf("Super Trunfo \n");
+    printf("Escolha o número de um atributo entre as opções abaixo:\n");
+    printf("1-Populaçao\n");
+    printf("2-Area\n");
+    printf("3- Densidade\n");
+    printf("4-número de pontos turisticos\n");
+    printf("5-Pib\n");
+
+    scanf("%d", &atributo);
+
+        if(atributo >= 1 && atributo <=5){
+            printf(" O nome do Estado da carta 1 é: %s\n", estado);
+            printf(" O nome do Estado da carta 2 é: %s\n", estado2);
+        }
        
-    }else {
-        printf ("carta 2 Rio Grande do Sul venceu");
-
+    switch (atributo)
+    {
+    case 1:
+       printf("O atributo escolhido foi populacao\n");
+       printf(" A população da carta 1 é : %f\n", populacao);
+       printf(" A população da carta 2 é: %f\n", populacao2);
+       if(populacao==populacao2){
+        printf("empate");
+    }else{
+        if(populacao>populacao2){
+            printf("o vencedor é a carta 1");
+        }else{
+            printf("o vencedor é a carta 2");
+        }
     }
+        break;
 
+    case 2:
+        printf(" o atributo escolhido foi area\n");
+        printf(" A area da carta 1 é : %f\n", area);
+        printf(" A area da carta 2 e: %f\n", area2);
+        if(area==area2){
+            printf("empate");
+        }else{
+            if(area>area2){
+                printf("o vencedor é a carta 1");
+            }else{
+                printf("o vencedor é a carta 2");
+            }
+        }
+        break;
+
+    case 3:
+    printf(" O atributo escolhido foi densidade\n");
+        printf("A densidade da carta 1 é: %f\n", densidadeCarta1);
+        printf(" A densidade da carta 2 é : %f\n", densidadeCarta2);
+        if(densidadeCarta1==densidadeCarta2){
+            printf("empate");
+        }else{
+            if(densidadeCarta1<densidadeCarta2){
+                printf("o vencedor é a carta 1");
+            }else{
+                printf("o vencedor é a carta 2");
+            }
+        }
+         break;
+
+    case 4:
+        printf(" O atributo escolhido foi turismo\n");
+        printf(" A quantidade de pontos turisticos da carta 1 é: %d\n", turismo);
+        printf(" A quantidade de pontos turisticos da carta 2 é: %d\n", turismo2);
+
+        if(turismo==turismo2){
+            printf("empate");
+        }else{
+            if(turismo>turismo2){
+                printf("o vencedor é a carta 1");
+            }else{
+                printf("o vencedor é a carta 2");
+            }
+        }
+        break;
+
+    case 5:
+        printf(" o atributo escolhido foi Pib Per Capita\n");
+        printf(" O valor do PIB per capita da carta 1 é: %f\n", pibPerCapitaCarta1);
+        printf(" O valor do PIB per capita da carta 2 é: %f\n", pibPerCapitaCarta2);
+        if(pibPerCapitaCarta1==pibPerCapitaCarta2){
+            printf("empate");
+        }else{
+            if(pibPerCapitaCarta1>pibPerCapitaCarta2){
+                printf("o vencedor é a carta 1");
+            }else{
+                printf("o vencedor é a carta 2");
+            }
+        }
+        break;
+
+    default:
+        printf("Este numero não é válido\n");
+        break;
+    }
     
     return 0;
 
